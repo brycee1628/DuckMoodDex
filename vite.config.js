cf3cfdc2467448def8942ff9ef48e21dbb27874c
Bryce <a0955361101@gmail.com>
2025-03-19 11:17:39 +0800

    change base url

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/palworld/",
-  plugins: [
-    vue(),
-  ],
-  server: {
-    port: 3000,
-    open: true,
-  },
-})
+    base: "/DuckMoodDex/",
+    plugins: [vue()],
+    server: {
+        port: 3000,
+        open: true,
+    },
+});
